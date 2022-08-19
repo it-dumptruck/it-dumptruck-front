a3f35919b4fd09709eb8361dfa99a2bd00a3ee08
YouHoGeon <me@youhogeon.com>
2022-08-19 15:09:56 +0900

    스타일 변경: 버튼에 border 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default ({ className, children }: any) => {
     return (
-        <button className={`bg-zinc-100 px-4 py-2 text-sm rounded shadow hover:shadow-md transition-all hover:bg-zinc-200 ${className}`} role="button">{ children }</button>
+        <button className={`bg-zinc-100 px-4 py-2 border text-sm rounded shadow hover:shadow-md transition-all hover:bg-zinc-200 ${className}`} role="button">{ children }</button>
     );
 };
