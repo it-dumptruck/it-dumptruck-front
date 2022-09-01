8acb922e50fe5573c9705f380d92688cc4678336
YouHoGeon <me@youhogeon.com>
2022-09-01 18:34:30 +0900

    add: meta tages, nav tag

--- src/components/DumpList.tsx
+++ src/components/DumpList.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { IDump, IDumpItem } from '../api/types';
 import DumpItem from './DumpItem';
 
@@ -12,12 +13,23 @@ const DumpList: React.FC<DumpListProps> = ({ dumps }) => {
     })
     return (
         <div>
+            <nav className="relative">
+                {
+                    dumps.map((item: IDump) => {
+                        return (
+                            item.dumps.map((dump: IDumpItem) => {
+                                return <Link to={`/dumps/${dump.dumpID}`} className="absolute -top-96 -left-96 focus:static">{ item.groupName + ' ' + dump.dumpName }</Link>
+                            })
+                        )
+                    })
+                }
+            </nav>
             {
                 dumps.map((item: IDump) => {
                     return (
-                    <div className="mt-10 first:mt-0" aria-labelledby={"dumpgroup-" + item.groupName.toLowerCase()}>
+                    <div className="mb-10 last:mb-0" aria-labelledby={"dumpgroup-" + item.groupName.toLowerCase()}>
                         <h3 className="text-2xl font-bold" id={"dumpgroup-" + item.groupName.toLowerCase()}>{item.groupName}</h3>
-                        
+
                         <div className="grid grid-cols-1 xl:grid-cols-2 -mx-2">
                             {
                                 item.dumps.map((dump: IDumpItem) => {
