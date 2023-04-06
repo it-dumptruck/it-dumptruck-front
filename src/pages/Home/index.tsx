ba0baa0461883f78f786aa4a433e3e14db266744
jaechan <penguin_03@naver.com>
2023-04-06 23:08:15 +0900

    merge

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,10 @@
 import axios from 'axios';
 import React, { Suspense, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
-<<<<<<< HEAD
-import { useMatch, useNavigate, useParams, useRoutes } from 'react-router-dom';
-import { isIfStatement } from 'typescript';
-import client, { getAuth, getDumpsLists, setToken, setUid } from '../../api';
-import { IDump } from '../../api/types';
-import Ad from '../../components/Ad';
-=======
->>>>>>> master
 import DumpList from '../../components/DumpList';
 import Loading from '../../components/Loading';
 import useDumps from '../../hooks/useDumps';
+import Ad from '../../components/Ad';
 const HomePage = () => {
 
     const { data: dumps,isLoading, isSuccess} = useDumps();
@@ -24,12 +17,7 @@ const HomePage = () => {
                 isLoading ? <Loading title="덤프 목록을 불러오는 중.." /> :
                 isSuccess ? <DumpList dumps={ dumps } /> : <Loading title="덤프 목록을 불러올 수 없습니다. 다시 시도해주세요." />
             }
-<<<<<<< HEAD
-            <Ad />
-        </DefaultTemplate>
-=======
         </>
->>>>>>> master
     );
 };
 
